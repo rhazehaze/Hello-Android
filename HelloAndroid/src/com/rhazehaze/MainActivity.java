4feb30a763d1629ca2d6bbb4085a485dcd191a4c
rhazehaze <rhazehaze@gmail.com>
2013-10-21 11:44:33 +0700

    Add Comment

--- HelloAndroid/src/com/rhazehaze/MainActivity.java
+++ HelloAndroid/src/com/rhazehaze/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	
-	//Add Comment
+	//Add Comment Rheno
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
